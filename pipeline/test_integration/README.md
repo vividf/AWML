27f9cf89a1b1af0c4c738431db27c2286b881954
Satoshi Tanaka <16330533+scepter914@users.noreply.github.com>
2024-06-27 15:13:22 +0900

    fix(pipelines): change directory name from pipeline to pipelines
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- pipeline/test_integration/README.md
+++
@@ -1,7 +0,0 @@
-# test_integration
-
-- Run integration test on local
-
-```
-pipeline/test_integration/scripts/test.sh
-```
--- pipeline/test_integration/scripts/test.sh
+++
@@ -1,6 +0,0 @@
-# Test create data for T4dataset
-python pipeline/detection3d/create_data_t4dataset.py --root_path ./data/t4dataset --config pipeline/test_integration/configs/dataset/test.py --version xx1 --max_sweeps 2 --out_dir ./data/t4dataset/info/test_name && \
-# Test training for TransFusion
-python pipeline/detection3d/train.py pipeline/test_integration/configs/TransFusion/transfusion_lidar_pillar_second_secfpn_1xb1-cyclic-20e_t4xx1_test.py && \
-# Test eval for TransFusion
-python pipeline/detection3d/test.py pipeline/test_integration/configs/TransFusion/transfusion_lidar_pillar_second_secfpn_1xb1-cyclic-20e_t4xx1_test.py work_dirs/epoch_20.pth
