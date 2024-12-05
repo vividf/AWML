42eb28535ab3856b734e74c5bca488a81b9b4a22
Shintaro Tomie <58775300+Shin-kyoto@users.noreply.github.com>
2024-12-05 13:55:00 +0900

    fix(auto_labeling_3d): use gravity_center for xyz of bbox
    Signed-off-by: Shin-kyoto <aquashin0202@gmail.com>

--- tools/auto_labeling_3d/create_info/inference.py
+++ tools/auto_labeling_3d/create_info/inference.py
@@ -3,6 +3,8 @@ import pickle
 from typing import List, Dict, Tuple, Any
 import uuid
 
+import numpy as np
+
 from mmdet3d.registry import MODELS
 from mmdet3d.structures import LiDARInstance3DBoxes
 from mmengine.config import Config
@@ -11,7 +13,7 @@ from mmengine.runner import Runner, autocast, load_checkpoint
 from mmengine.utils import ProgressBar
 from numpy.typing import NDArray
 
-def _predict_one_frame(model: Any, data: Dict[str, Any]) -> Tuple[LiDARInstance3DBoxes, NDArray, NDArray]:
+def _predict_one_frame(model: Any, data: Dict[str, Any]) -> Tuple[NDArray, NDArray, NDArray]:
     """
     Predict pseudo label for one frame.
     """
@@ -20,14 +22,25 @@ def _predict_one_frame(model: Any, data: Dict[str, Any]) -> Tuple[LiDARInstance3
 
     # get bboxes, scores, labels
     bboxes: NDArray = outputs[0].pred_instances_3d["bboxes_3d"].tensor.detach().cpu()
-    scores: NDArray = outputs[0].pred_instances_3d["scores_3d"].detach().cpu()
-    labels: NDArray = outputs[0].pred_instances_3d["labels_3d"].detach().cpu()
+    scores: NDArray = outputs[0].pred_instances_3d["scores_3d"].detach().cpu().numpy()
+    labels: NDArray = outputs[0].pred_instances_3d["labels_3d"].detach().cpu().numpy()
 
     bboxes = LiDARInstance3DBoxes(bboxes, box_dim=9)
+    box_gravity_centers: NDArray = bboxes.gravity_center.numpy().astype(np.float64)
+    box_dims: NDArray = bboxes.dims.numpy().astype(np.float64)
+    box_yaws: NDArray = bboxes.yaw.numpy().astype(np.float64)
+    box_velocities: NDArray = bboxes.tensor.numpy().astype(np.float64)[:, 7:9]
+
+    bboxes = np.concatenate([
+        box_gravity_centers, # (N, 3) [x, y, z]
+        box_dims,            # (N, 3) [x_size(length), y_size(width), z_size(height)]
+        box_yaws[:, None],   # (N, 1) [yaw]
+        box_velocities,      # (N, 2) [vx, vy]
+    ], axis=1)
 
     return bboxes, scores, labels
 
-def _results_to_info(non_annotated_dataset_info: Dict[str, Any], inference_results: List[Tuple[LiDARInstance3DBoxes, NDArray, NDArray]]) -> Dict[str, Any]:
+def _results_to_info(non_annotated_dataset_info: Dict[str, Any], inference_results: List[Tuple[NDArray, NDArray, NDArray]]) -> Dict[str, Any]:
     """
     Convert non annotated dataset info and inference results to pseudo labeled info.
     """
@@ -37,11 +50,13 @@ def _results_to_info(non_annotated_dataset_info: Dict[str, Any], inference_resul
         for instance_index, (bbox, score, label) in enumerate(zip(bboxes, scores, labels)):
             pred_instance_3d: Dict[str, Any] = {}
             
+            # [x, y, z, x_size(length), y_size(width), z_size(height), yaw]
             pred_instance_3d["bbox_3d"]: List[float] = bbox[:7].tolist()
+            # [vx, vy]
             pred_instance_3d["velocity"]: List[float] = bbox[7:9].tolist()
             pred_instance_3d["instance_id_3d"]: str = str(uuid.uuid4())
-            pred_instance_3d["bbox_label_3d"]: int = label.item()
-            pred_instance_3d["bbox_score_3d"]: float = score.item()
+            pred_instance_3d["bbox_label_3d"]: int = int(label)
+            pred_instance_3d["bbox_score_3d"]: float = float(score)
 
             pred_instances_3d.append(pred_instance_3d)
         non_annotated_dataset_info["data_list"][frame_index]["pred_instances_3d"] = pred_instances_3d
