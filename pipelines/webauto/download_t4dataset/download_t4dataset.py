e269dd86c34d603d1f34df42d4345ce4610dabfd
Satoshi Tanaka <16330533+scepter914@users.noreply.github.com>
2024-12-27 15:49:50 +0900

    fix(t4dataset_download): refactoring
    Signed-off-by: scepter914 <scepter914@gmail.com>
    Co-authored-by: kminoda <44218668+kminoda@users.noreply.github.com>

--- pipelines/webauto/download_t4dataset/download_t4dataset.py
+++ pipelines/webauto/download_t4dataset/download_t4dataset.py
@@ -14,11 +14,32 @@ from typing import Union
 import yaml
 
 
+def get_t4dataset_ids(config_path: str) -> list[str]:
+    """
+    Get T4Dataset IDs like "0df0328e-39ea-42f1-844a-b455c91dc6cc".
+
+    Args:
+        config_path (str): The path to config.
+
+    Returns: list[str]
+    """
+    with open(config_path) as f:
+        data_splits = yaml.safe_load(f)
+    required_keys = ["train", "val", "test"]
+    assert isinstance(data_splits, dict) and all(
+        [isinstance(data_splits[key], list) for key in required_keys]
+    ), "config file must be a type of `dict[str, list]`"
+    return sum([data_splits[key] for key in required_keys], [])
+
+
 def divide_file_path(full_path: str) -> Union[str, str, str, str, str]:
     """
-    Args
-        full_path (str): './dir/subdir/filename.ext.ext2'
-    Return
+    Get each element of path for file path.
+
+    Args:
+        full_path (str): "./dir/subdir/filename.ext.ext2"
+
+    Return:
         ["./dir/subdir", "filename.ext", "subdir", "filename" "ext.ext2" ]
     """
     dir_name, base_name = os.path.split(full_path)
@@ -29,91 +50,154 @@ def divide_file_path(full_path: str) -> Union[str, str, str, str, str]:
 
 
 def check_t4dataset_latest_version(
+    webauto_path: str,
     project_id: str,
     t4dataset_id: str,
 ) -> int:
-    """Return the latest version of t4dataset using `webauto data annotation-dataset describe` command.
+    """
+    Return the latest version of t4dataset using `webauto data annotation-dataset describe` command.
+
     Args:
+        webauto_path (str): The path to WebAutoCLI.
         project_id (str): The project id of webauto command.
         t4dataset_id (str): The t4dataset id of webauto command.
+    Return: The latest version of T4dataset (int)
     """
+
     describe_command = (
-        "webauto data annotation-dataset describe --project-id {} --annotation-dataset-id {} --output json".format(
-            project_id, t4dataset_id
+        "{} data annotation-dataset describe --project-id {} --annotation-dataset-id {} --output json".format(
+            webauto_path, project_id, t4dataset_id
         )
     )
-    result = json.loads(
-        subprocess.run(
-            describe_command,
-            shell=True,
-            check=True,
-            capture_output=True,
-            text=True,
-        ).stdout
-    )
-    return result["version_id"]
+    result = subprocess.run(
+        describe_command,
+        shell=True,
+        check=True,
+        capture_output=True,
+        text=True,
+    ).stdout
+    if result == "" or result == "\n":
+        print(f"fail to check the version in {describe_command}")
+    result_json = json.loads(result)
+    return result_json["version_id"]
 
 
-def download_t4dataset(
+def pull_t4dataset(
+    webauto_path: str,
     project_id: str,
     t4dataset_id: str,
     output_dir: str,
-    config_path: str,
-    delete_rosbag: bool,
 ) -> None:
-    """Download t4dataset using webauto CLI.
-    When there are multiple versions in t4dataset, it would automatically
-    Return: None
+    """
+    Pull T4dataset using WebAutoCLI.
 
     Args:
+        webauto_path (str): The path to WebAutoCLI.
         project_id (str): The project id of webauto command.
         t4dataset_id (str): The t4dataset id of webauto command.
         output_dir (str): The output directory path.
-        config_path (str): The path to config.
+    Return: None
     """
-    download_command = "webauto data annotation-dataset pull --project-id {} --annotation-dataset-id {} --asset-dir {}"
 
-    with TemporaryDirectory() as temp_dir:
-        t4dataset_version_id: int = check_t4dataset_latest_version(project_id, t4dataset_id)
-        print(
-            f"\n***************** start downloading t4dataset id {t4dataset_id} with version id {t4dataset_version_id}"
-        )
+    download_command = "{} data annotation-dataset pull --project-id {} --annotation-dataset-id {} --asset-dir {}"
+    download_command_ = download_command.format(
+        webauto_path,
+        project_id,
+        t4dataset_id,
+        output_dir,
+    )
+    print(download_command_)
+    subprocess.call(download_command_, shell=True)
 
-        _, _, _, database_name, _ = divide_file_path(config_path)
-        from_directory = os.path.join(
-            temp_dir,
-            "annotation_dataset",
-            t4dataset_id,
-            str(t4dataset_version_id),
-        )
-        to_directory = os.path.join(output_dir, database_name, t4dataset_id)
 
-        # skip if the target t4dataset already exists
-        to_directory_with_version_id = os.path.join(to_directory, str(t4dataset_version_id))
-        if os.path.exists(to_directory_with_version_id):
-            print(f"t4dataset already exists at {to_directory_with_version_id}")
-            return
+def move_t4dataset(
+    from_directory: str,
+    to_directory: str,
+) -> None:
+    """
+    Move temp directory to local directory for T4dataset.
 
-        download_command_ = download_command.format(
-            project_id,
-            t4dataset_id,
-            temp_dir,
-        )
-        print(download_command_)
-        subprocess.call(download_command_, shell=True)
+    Args:
+        from_directory (str): Path to move from
+        to_directory (str): Path to move to
+    Returns: None
+    """
 
-        if delete_rosbag is True:
-            rosbag_path: str = os.path.join(from_directory, "input_bag")
-            rm_command = "rm -r {}".format(rosbag_path)
-            print(rm_command)
-            subprocess.call(rm_command, shell=True)
+    os.makedirs(to_directory, exist_ok=True)
+    mv_command = "mv {} {}".format(from_directory, to_directory)
+    print(mv_command)
+    subprocess.call(mv_command, shell=True)
 
-        # rename directory
-        os.makedirs(to_directory, exist_ok=True)
 
-        mv_command = "mv {} {}".format(from_directory, to_directory)
-        print(mv_command)
-        subprocess.call(mv_command, shell=True)
+def download_t4dataset(
+    config_path: Path,
+    webauto_path: str,
+    project_id: str,
+    t4dataset_id: str,
+    output_dir: str,
+    temp_dir: str,
+    delete_rosbag: bool,
+) -> None:
+    """
+    Download T4dataset.
+
+    Args:
+        config_path (Path): The path to the config file.
+        webauto_path (str): The path to WebAutoCLI.
+        project_id (str): The project id of webauto command.
+        t4dataset_id (str): The t4dataset id of webauto command.
+        output_dir (str): The output directory path.
+        temp_dir (str): The temp directory to download T4dataset.
+        delete_rosbag: (bool): If the this value is true, the rosbag file is deleted.
+    Return: None
+    """
+
+    # check the latest version of T4dataset
+    t4dataset_version_id: int = check_t4dataset_latest_version(
+        webauto_path,
+        project_id,
+        t4dataset_id,
+    )
+
+    print(f"\n***** Start downloading T4Dataset ID:{t4dataset_id} with version:{t4dataset_version_id}")
+
+    # set path to directory
+    _, _, _, database_name, _ = divide_file_path(config_path)
+    from_directory = os.path.join(
+        temp_dir,
+        "annotation_dataset",
+        t4dataset_id,
+        str(t4dataset_version_id),
+    )
+    to_directory = os.path.join(output_dir, database_name, t4dataset_id)
+    to_directory_with_version_id = os.path.join(to_directory, str(t4dataset_version_id))
+
+    # skip if the target t4dataset already exists
+    if os.path.exists(to_directory_with_version_id):
+        print(f"t4dataset already exists at {to_directory_with_version_id}")
+        return
+
+    pull_t4dataset(
+        webauto_path,
+        project_id,
+        t4dataset_id,
+        temp_dir,
+    )
+
+    # delete rosbag
+    if delete_rosbag is True:
+        rosbag_path: str = os.path.join(from_directory, "input_bag")
+        rm_command = "rm -r {}".format(rosbag_path)
+        print(rm_command)
+        subprocess.call(rm_command, shell=True)
+
+    # check if the dataset is successfully downloaded
+    path_to_annotation = os.path.join(from_directory, "annotation")
+    if not os.path.isdir(path_to_annotation):
+        print(f"fail to download annotation files at {path_to_annotation}")
+        return
+
+    move_t4dataset(from_directory, to_directory)
 
 
 def parse_args():
@@ -141,6 +225,12 @@ def parse_args():
         action="store_true",
         help="Delete rosbag file from T4dataset",
     )
+    parser.add_argument(
+        "--webauto-path",
+        type=str,
+        help="The path to WebAutoCLI binary file for executing the CLI command",
+        default="webauto",
+    )
     args = parser.parse_args()
     return args
 
@@ -150,26 +240,22 @@ def main():
 
     config_path = Path(args.config)
     assert config_path.exists() and config_path.is_file()
-
     output_dir = Path(args.output)
     assert output_dir.exists() and output_dir.is_dir()
 
-    with open(config_path) as f:
-        data_splits = yaml.safe_load(f)
-    required_keys = ["train", "val", "test"]
-    assert isinstance(data_splits, dict) and all(
-        [isinstance(data_splits[key], list) for key in required_keys]
-    ), "config file must be a type of `dict[str, list]`"
-    t4dataset_ids = sum([data_splits[key] for key in required_keys], [])
+    t4dataset_ids = get_t4dataset_ids(config_path)
 
     for t4dataset_id in t4dataset_ids:
-        download_t4dataset(
-            args.project_id,
-            t4dataset_id,
-            output_dir,
-            config_path,
-            args.delete_rosbag,
-        )
+        with TemporaryDirectory() as temp_dir:
+            download_t4dataset(
+                config_path,
+                args.webauto_path,
+                args.project_id,
+                t4dataset_id,
+                output_dir,
+                temp_dir,
+                args.delete_rosbag,
+            )
 
 
 if __name__ == "__main__":
