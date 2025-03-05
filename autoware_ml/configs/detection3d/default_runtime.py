7c88ff04c2fd0a526f3a19d6ef44287982661fdf
Kenzo Lobos Tsunekawa <kenzo.lobos@tier4.jp>
2025-03-05 12:21:39 +0900

    fix(nccl): increased nccl timeout (#11)
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>

--- autoware_ml/configs/detection3d/default_runtime.py
+++ autoware_ml/configs/detection3d/default_runtime.py
@@ -12,7 +12,7 @@ default_hooks = dict(
 env_cfg = dict(
     cudnn_benchmark=False,
     mp_cfg=dict(mp_start_method="fork", opencv_num_threads=0),
-    dist_cfg=dict(backend="nccl"),
+    dist_cfg=dict(backend="nccl", timeout=7200),
 )
 
 log_processor = dict(type="LogProcessor", window_size=50, by_epoch=True)
