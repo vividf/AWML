8a1fa1e70ac681c6e35ad8bcfe0a83077e290aa9
vividf <yihsiang.fang@tier4.jp>
2025-07-16 15:11:34 +0900

    chore: fix visualization hook
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- tools/classification2d/result_visualization_hook.py
+++ tools/classification2d/result_visualization_hook.py
@@ -2,4 +2,4 @@ debug = True  # 或 False
 
 custom_hooks = []
 if debug:
-    custom_hooks.append(dict(type="ResultVisualizationHook", save_dir="./projection_vis/"))
+    custom_hooks.append(dict(type="ResultVisualizationHook", save_dir="./projection_vis_origin/"))
