694200a16bd0cc352fd5020c440693c822a45073
vividf <yihsiang.fang@tier4.jp>
2025-11-11 22:14:54 +0900

    chore: rename file
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- autoware_ml/deployment/runners/__init__.py
+++ autoware_ml/deployment/runners/__init__.py
@@ -1,6 +1,6 @@
 """Deployment runners for unified deployment workflow."""
 
-from .unified_runner import DeploymentRunner
+from .deployment_runner import DeploymentRunner
 
 __all__ = [
     "DeploymentRunner",
