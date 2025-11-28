d51f32f5a8f74b4f4673663edf7de74e04af6e35
vividf <yihsiang.fang@tier4.jp>
2025-11-28 13:45:27 +0900

    chore: comment unused import
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- deployment/runners/__init__.py
+++ deployment/runners/__init__.py
@@ -4,9 +4,11 @@ from deployment.runners.common.artifact_manager import ArtifactManager
 from deployment.runners.common.deployment_runner import BaseDeploymentRunner
 from deployment.runners.common.evaluation_orchestrator import EvaluationOrchestrator
 from deployment.runners.common.verification_orchestrator import VerificationOrchestrator
-from deployment.runners.projects.calibration_runner import CalibrationDeploymentRunner
+
+# from deployment.runners.projects.calibration_runner import CalibrationDeploymentRunner
 from deployment.runners.projects.centerpoint_runner import CenterPointDeploymentRunner
-from deployment.runners.projects.yolox_runner import YOLOXOptElanDeploymentRunner
+
+# from deployment.runners.projects.yolox_runner import YOLOXOptElanDeploymentRunner
 
 __all__ = [
     # Base runner
