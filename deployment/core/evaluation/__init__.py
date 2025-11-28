85f208dd885af6c0dbbd7540d0926d8e59967f6f
vividf <yihsiang.fang@tier4.jp>
2025-11-28 11:54:57 +0900

    chore: remove unused class
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- deployment/core/evaluation/__init__.py
+++ deployment/core/evaluation/__init__.py
@@ -6,17 +6,6 @@ from deployment.core.evaluation.evaluator_types import (
     ModelSpec,
     VerifyResultDict,
 )
-from deployment.core.evaluation.results import (
-    ClassificationEvaluationMetrics,
-    ClassificationResult,
-    Detection2DEvaluationMetrics,
-    Detection2DResult,
-    Detection3DEvaluationMetrics,
-    Detection3DResult,
-    EvaluationMetrics,
-    LatencyStats,
-    StageLatencyBreakdown,
-)
 from deployment.core.evaluation.verification_mixin import VerificationMixin
 
 __all__ = [
@@ -25,14 +14,5 @@ __all__ = [
     "EvalResultDict",
     "ModelSpec",
     "VerifyResultDict",
-    "ClassificationResult",
-    "Detection2DResult",
-    "Detection3DResult",
-    "ClassificationEvaluationMetrics",
-    "Detection2DEvaluationMetrics",
-    "Detection3DEvaluationMetrics",
-    "EvaluationMetrics",
-    "LatencyStats",
-    "StageLatencyBreakdown",
     "VerificationMixin",
 ]
