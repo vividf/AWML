b7b36ee6e9e8ffd5fcfba05e66320e652304ba57
vividf <yihsiang.fang@tier4.jp>
2025-12-02 15:32:58 +0900

    chore: clean code
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- deployment/exporters/centerpoint/__init__.py
+++ deployment/exporters/centerpoint/__init__.py
@@ -1,35 +1,12 @@
-"""CenterPoint-specific exporter workflows and constants."""
+"""CenterPoint-specific exporter workflows and config accessors."""
 
 from deployment.exporters.centerpoint.onnx_workflow import CenterPointONNXExportWorkflow
 from deployment.exporters.centerpoint.tensorrt_workflow import CenterPointTensorRTExportWorkflow
 from projects.CenterPoint.deploy.configs.deploy_config import model_io, onnx_config
 
-# Re-export structured configs for direct access
-OUTPUT_NAMES = model_io["head_output_names"]
-
-# Component configs
-_voxel_cfg = onnx_config["components"]["voxel_encoder"]
-_backbone_cfg = onnx_config["components"]["backbone_head"]
-
-VOXEL_ENCODER_NAME = _voxel_cfg["name"]
-VOXEL_ENCODER_ONNX = _voxel_cfg["onnx_file"]
-VOXEL_ENCODER_ENGINE = _voxel_cfg["engine_file"]
-
-BACKBONE_HEAD_NAME = _backbone_cfg["name"]
-BACKBONE_HEAD_ONNX = _backbone_cfg["onnx_file"]
-BACKBONE_HEAD_ENGINE = _backbone_cfg["engine_file"]
-
 __all__ = [
-    # Workflows
     "CenterPointONNXExportWorkflow",
     "CenterPointTensorRTExportWorkflow",
-    # Model architecture constants
-    "OUTPUT_NAMES",
-    # Export file structure constants
-    "VOXEL_ENCODER_NAME",
-    "BACKBONE_HEAD_NAME",
-    "VOXEL_ENCODER_ONNX",
-    "BACKBONE_HEAD_ONNX",
-    "VOXEL_ENCODER_ENGINE",
-    "BACKBONE_HEAD_ENGINE",
+    "model_io",
+    "onnx_config",
 ]
