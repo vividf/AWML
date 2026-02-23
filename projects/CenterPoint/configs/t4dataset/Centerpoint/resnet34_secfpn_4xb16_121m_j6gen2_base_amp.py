8ecc07f64e3f375b4a6968820c69d4407faab86d
vividf <yihsiang.fang@tier4.jp>
2026-02-23 15:27:18 +0900

    chore: update resnet j6gen2
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- projects/CenterPoint/configs/t4dataset/Centerpoint/resnet34_secfpn_4xb16_121m_j6gen2_base_amp.py
+++ projects/CenterPoint/configs/t4dataset/Centerpoint/resnet34_secfpn_4xb16_121m_j6gen2_base_amp.py
@@ -1,298 +1,74 @@
-_base_ = [
-    "../../../../../autoware_ml/configs/detection3d/default_runtime.py",
-    "../../../../../autoware_ml/configs/detection3d/dataset/t4dataset/j6gen2_base.py",
-    "../../default/second_secfpn_base.py",
-]
-custom_imports = dict(imports=["projects.CenterPoint.models"], allow_failed_imports=False)
-custom_imports["imports"] += _base_.custom_imports["imports"]
-custom_imports["imports"] += ["autoware_ml.detection3d.datasets.transforms"]
-custom_imports["imports"] += ["autoware_ml.hooks"]
-# custom_imports["imports"] += ["autoware_ml.backends.mlflowbackend"]
-custom_imports["imports"] += ["autoware_ml.samplers"]
+_base_ = ["./second_secfpn_4xb16_121m_j6gen2_base_amp.py"]
 
-# This is a base file for t4dataset, add the dataset config.
-# type, data_root and ann_file of data.train, data.val and data.test
-point_cloud_range = [-122.40, -122.40, -3.0, 122.40, 122.40, 5.0]
-voxel_size = [0.24, 0.24, 8.0]
-grid_size = [1020, 1020, 1]  # (122.40 / 0.24 == 510, 510 * 2 == 1020)
-sweeps_num = 1
-input_modality = dict(
-    use_lidar=True,
-    use_camera=False,
-    use_radar=False,
-    use_map=False,
-    use_external=False,
-)
-out_size_factor = 2
-
-backend_args = None
-# backend_args = dict(backend="disk")
-point_load_dim = 5  # x, y, z, intensity, ring_id
-point_use_dim = 3  # x, y, z
-lidar_sweep_dims = [0, 1, 2, 3, 4]
-
-# eval parameter
-eval_class_range = {
-    "car": 121,
-    "truck": 121,
-    "bus": 121,
-    "bicycle": 121,
-    "pedestrian": 121,
-}
-
-# user setting
-data_root = "data/t4dataset/"
-info_directory_path = "info/"
-train_gpu_size = 4
-train_batch_size = 8
-test_batch_size = 2
-num_workers = 32
-val_interval = 1
-max_epochs = 30
-
-experiment_group_name = "centerpoint/j6gen2_base/" + _base_.dataset_type
-experiment_name = "second_secfpn_4xb16_121m_j6gen2_base_amp"
+# ResNet34 backbone variant - only override what differs from SECOND base
+experiment_name = "resnet34_secfpn_4xb16_121m_j6gen2_base_amp"
 work_dir = "work_dirs/" + experiment_group_name + "/" + experiment_name
 
-train_pipeline = [
-    dict(
-        type="LoadPointsFromFile",
-        coord_type="LIDAR",
-        load_dim=point_load_dim,
-        use_dim=point_load_dim,
-        backend_args=backend_args,
-    ),
-    dict(
-        type="LoadPointsFromMultiSweeps",
-        sweeps_num=sweeps_num,
-        load_dim=point_load_dim,
-        use_dim=lidar_sweep_dims,
-        pad_empty_sweeps=True,
-        remove_close=True,
-        backend_args=backend_args,
-    ),
-    dict(type="LoadAnnotations3D", with_bbox_3d=True, with_label_3d=True),
-    dict(
-        type="RandomFlip3D",
-        sync_2d=False,
-        flip_ratio_bev_horizontal=0.5,
-        flip_ratio_bev_vertical=0.5,
-    ),
-    dict(
-        type="GlobalRotScaleTrans",
-        rot_range=[-1.571, 1.571],
-        scale_ratio_range=[0.80, 1.20],
-        translation_std=[1.0, 1.0, 0.2],
-    ),
-    dict(type="PointsRangeFilter", point_cloud_range=point_cloud_range),
-    dict(type="ObjectRangeFilter", point_cloud_range=point_cloud_range),
-    dict(type="ObjectNameFilter", classes={{_base_.class_names}}),
-    dict(type="ObjectMinPointsFilter", min_num_points=5),
-    dict(type="PointShuffle"),
-    dict(type="Pack3DDetInputs", keys=["points", "gt_bboxes_3d", "gt_labels_3d"]),
-]
+# ResNet34 uses smaller batch size (8 vs 16) due to larger backbone memory
+train_batch_size = 8
+train_dataloader = dict(batch_size=train_batch_size)
 
-test_pipeline = [
-    dict(
-        type="LoadPointsFromFile",
-        coord_type="LIDAR",
-        load_dim=point_load_dim,
-        use_dim=point_load_dim,
-        backend_args=backend_args,
-    ),
-    dict(
-        type="LoadPointsFromMultiSweeps",
-        sweeps_num=sweeps_num,
-        load_dim=point_load_dim,
-        use_dim=lidar_sweep_dims,
-        pad_empty_sweeps=True,
-        remove_close=True,
-        backend_args=backend_args,
-        test_mode=True,
-    ),
-    dict(type="PointsRangeFilter", point_cloud_range=point_cloud_range),
-    dict(
-        type="Pack3DDetInputs",
-        keys=["points", "gt_bboxes_3d", "gt_labels_3d"],
-        # Specify the metadata keys required by the downstream pipeline.
-        # Refer to the official MMDetection3D formatting transform implementation for details:
-        # https://github.com/open-mmlab/mmdetection3d/blob/main/mmdet3d/datasets/transforms/formating.py#L67
-        # Also see the content structure in "t4dataset_base_infos_test.pkl" for reference.
-        meta_keys=(
-            "timestamp",
-            "lidar2img",
-            "depth2img",
-            "cam2img",
-            "box_type_3d",
-            "sample_idx",
-            "sample_token",
-            "lidar_path",
-            "ori_cam2img",
-            "cam2global",
-            "lidar2cam",
-            "ego2global",
-            "city",
-            "vehicle_type",
-        ),
-    ),
-]
+# Disable MLflow backend (optional - uncomment in base if needed)
+custom_imports = dict(imports=_base_.custom_imports["imports"].copy(), allow_failed_imports=False)
+custom_imports["imports"] = [x for x in custom_imports["imports"] if "mlflow" not in x]
 
-# construct a pipeline for data and gt loading in show function
-# please keep its loading function consistent with test_pipeline (e.g. client)
-eval_pipeline = [
-    dict(
-        type="LoadPointsFromFile",
-        coord_type="LIDAR",
-        load_dim=point_load_dim,
-        use_dim=point_load_dim,
-        backend_args=backend_args,
-    ),
-    dict(
-        type="LoadPointsFromMultiSweeps",
-        sweeps_num=sweeps_num,
-        load_dim=point_load_dim,
-        use_dim=lidar_sweep_dims,
-        pad_empty_sweeps=True,
-        remove_close=True,
-        backend_args=backend_args,
-        test_mode=True,
-    ),
-    dict(type="PointsRangeFilter", point_cloud_range=point_cloud_range),
-    dict(
-        type="Pack3DDetInputs",
-        keys=["points", "gt_bboxes_3d", "gt_labels_3d"],
-        # Specify the metadata keys required by the downstream pipeline.
-        # Refer to the official MMDetection3D formatting transform implementation for details:
-        # https://github.com/open-mmlab/mmdetection3d/blob/main/mmdet3d/datasets/transforms/formating.py#L67
-        # Also see the content structure in "t4dataset_base_infos_test.pkl" for reference.
-        meta_keys=(
-            "timestamp",
-            "lidar2img",
-            "depth2img",
-            "cam2img",
-            "box_type_3d",
-            "sample_idx",
-            "sample_token",
-            "lidar_path",
-            "ori_cam2img",
-            "cam2global",
-            "lidar2cam",
-            "ego2global",
-            "city",
-            "vehicle_type",
-        ),
-    ),
-]
 
-train_dataloader = dict(
-    batch_size=train_batch_size,
-    num_workers=num_workers,
-    persistent_workers=True,
-    sampler=dict(type="DefaultSampler", shuffle=True),
-    dataset=dict(
-        type=_base_.dataset_type,
-        pipeline=train_pipeline,
-        modality=input_modality,
-        backend_args=backend_args,
-        data_root=data_root,
-        ann_file=info_directory_path + _base_.info_train_file_name,
-        metainfo=_base_.metainfo,
-        class_names=_base_.class_names,
-        test_mode=False,
-        data_prefix=_base_.data_prefix,
-        box_type_3d="LiDAR",
-    ),
-)
+lr = 3e-4
 
-val_dataloader = dict(
-    batch_size=test_batch_size,
-    num_workers=num_workers,
-    persistent_workers=True,
-    sampler=dict(type="DefaultSampler", shuffle=False),
-    dataset=dict(
-        type=_base_.dataset_type,
-        data_root=data_root,
-        ann_file=info_directory_path + _base_.info_val_file_name,
-        pipeline=test_pipeline,
-        metainfo=_base_.metainfo,
-        class_names=_base_.class_names,
-        modality=input_modality,
-        data_prefix=_base_.data_prefix,
-        test_mode=True,
-        box_type_3d="LiDAR",
-        backend_args=backend_args,
-    ),
-)
-test_dataloader = dict(
-    batch_size=test_batch_size,
-    num_workers=num_workers,
-    persistent_workers=True,
-    sampler=dict(type="DefaultSampler", shuffle=False),
-    dataset=dict(
-        type=_base_.dataset_type,
-        data_root=data_root,
-        ann_file=info_directory_path + _base_.info_test_file_name,
-        pipeline=test_pipeline,
-        metainfo=_base_.metainfo,
-        class_names=_base_.class_names,
-        modality=input_modality,
-        data_prefix=_base_.data_prefix,
-        test_mode=True,
-        box_type_3d="LiDAR",
-        backend_args=backend_args,
-    ),
-)
+# TODO(vividf): modify if needed
+# param_scheduler = [
+#     # learning rate scheduler
+#     # During the first (max_epochs * 0.3) epochs, learning rate increases from 0 to lr * 10
+#     # during the next epochs, learning rate decreases from lr * 10 to
+#     # lr * 1e-4
+#     dict(
+#         type="CosineAnnealingLR",
+#         T_max=8,
+#         eta_min=lr * 10,
+#         begin=0,
+#         end=8,
+#         by_epoch=True,
+#         convert_to_iter_based=True,
+#     ),
+#     dict(
+#         type="CosineAnnealingLR",
+#         T_max=22,
+#         eta_min=lr * 1e-4,
+#         begin=8,
+#         end=max_epochs,
+#         by_epoch=True,
+#         convert_to_iter_based=True,
+#     ),
+#     # momentum scheduler
+#     # During the first (0.3 * max_epochs) epochs, momentum increases from 0 to 0.85 / 0.95
+#     # during the next epochs, momentum increases from 0.85 / 0.95 to 1
+#     dict(
+#         type="CosineAnnealingMomentum",
+#         T_max=8,
+#         eta_min=0.85 / 0.95,
+#         begin=0,
+#         end=8,
+#         by_epoch=True,
+#         convert_to_iter_based=True,
+#     ),
+#     dict(
+#         type="CosineAnnealingMomentum",
+#         T_max=22,
+#         eta_min=1,
+#         begin=8,
+#         end=max_epochs,
+#         by_epoch=True,
+#         convert_to_iter_based=True,
+#     ),
+# ]
 
-val_evaluator = dict(
-    type="T4Metric",
-    data_root=data_root,
-    ann_file=data_root + info_directory_path + _base_.info_val_file_name,
-    metric="bbox",
-    backend_args=backend_args,
-    class_names={{_base_.class_names}},
-    name_mapping={{_base_.name_mapping}},
-    eval_class_range=eval_class_range,
-    filter_attributes=_base_.filter_attributes,
-)
+optimizer = dict(type="AdamW", lr=lr, weight_decay=0.01)
+clip_grad = dict(max_norm=1.0, norm_type=2)  # max norm of gradients upper bound to be 15 since amp is used
 
-test_evaluator = dict(
-    type="T4Metric",
-    data_root=data_root,
-    ann_file=data_root + info_directory_path + _base_.info_test_file_name,
-    metric="bbox",
-    backend_args=backend_args,
-    class_names={{_base_.class_names}},
-    name_mapping={{_base_.name_mapping}},
-    eval_class_range=eval_class_range,
-    filter_attributes=_base_.filter_attributes,
-    save_csv=True,
-)
 
+# Replace SECOND backbone with ResNet34 (BEVResNet)
+# eps=1e-5 for numerical stability in mixed precision
 model = dict(
-    data_preprocessor=dict(
-        type="Det3DDataPreprocessor",
-        voxel=True,
-        voxel_layer=dict(
-            max_num_points=32,
-            voxel_size=voxel_size,
-            point_cloud_range=point_cloud_range,
-            max_voxels=(96000, 96000),
-            deterministic=True,
-        ),
-    ),
-    pts_voxel_encoder=dict(
-        type="PillarFeatureNet",
-        in_channels=5,
-        feat_channels=[32, 32],
-        with_distance=False,
-        with_cluster_center=True,
-        with_voxel_center=True,
-        point_cloud_range=point_cloud_range,
-        voxel_size=voxel_size,
-        norm_cfg=dict(type="BN1d", eps=1e-3, momentum=0.01),
-        legacy=False,
-    ),
-    pts_middle_encoder=dict(type="PointPillarsScatter", in_channels=32, output_shape=(grid_size[0], grid_size[1])),
     pts_backbone=dict(
         _delete_=True,
         type="BEVResNet",  # Use custom BEV-friendly ResNet wrapper (renamed to avoid confusion)
@@ -309,12 +85,21 @@ model = dict(
         frozen_stages=-1,  # Don't freeze any stages initially
         base_channels=64,  # ResNet34 outputs: 64, 128, 256 channels (64*1, 64*2, 64*4)
         # ResNet34 uses BasicBlock (expansion=1), so base_channels=64 gives [64, 128, 256]
-        norm_cfg=dict(type="BN", eps=1e-3, momentum=0.01),
+        norm_cfg=dict(
+            type="BN", eps=1e-5, momentum=0.01
+        ),  # Fixed: eps changed from 1e-3 to 1e-5 for numerical stability
         norm_eval=False,  # Keep BN in training mode for better performance
         # Remove pretrained weights due to input channel mismatch (3 vs 32)
         # init_cfg=dict(type="Pretrained", checkpoint="torchvision://resnet34"),
         style="pytorch",
         in_channels=32,
+        # pretrained=True,
+        init_cfg=dict(
+            type="Pretrained",
+            checkpoint="work_dirs/resnet_34/resnet34_8xb32_mmcls.pth",
+            prefix="backbone.",  # Often needed to map keys correctly
+        ),
+        with_cp=True,
     ),
     pts_neck=dict(
         type="SECONDFPN",
@@ -331,108 +116,13 @@ model = dict(
         # stage2: (255, 255) -> upsample stride=2 -> (510, 510)
         # Final output: (510, 510) to match target size (grid_size // out_size_factor)
         upsample_strides=[0.5, 1, 2],  # Upsample to match target feature map size (510, 510)
-        norm_cfg=dict(type="BN", eps=0.001, momentum=0.01),
+        norm_cfg=dict(
+            type="BN", eps=1e-5, momentum=0.01
+        ),  # Fixed: eps changed from 0.001 (1e-3) to 1e-5 for numerical stability
         upsample_cfg=dict(type="deconv", bias=False),
         use_conv_for_no_stride=True,
     ),
-    pts_bbox_head=dict(
-        type="CenterHead",
-        in_channels=sum([128, 128, 128]),
-        tasks=[
-            dict(num_class=5, class_names=["car", "truck", "bus", "bicycle", "pedestrian"]),
-        ],
-        bbox_coder=dict(
-            voxel_size=voxel_size,
-            pc_range=point_cloud_range,
-            # No filter by range
-            post_center_range=[-200.0, -200.0, -10.0, 200.0, 200.0, 10.0],
-            out_size_factor=out_size_factor,
-        ),
-        # sigmoid(-4.595) = 0.01 for initial small values
-        separate_head=dict(type="CustomSeparateHead", init_bias=-4.595, final_kernel=1),
-        loss_cls=dict(type="mmdet.AmpGaussianFocalLoss", reduction="none", loss_weight=1.0),
-        loss_bbox=dict(type="mmdet.L1Loss", reduction="mean", loss_weight=0.25),
-        norm_bbox=True,
-    ),
-    train_cfg=dict(
-        pts=dict(
-            grid_size=grid_size,
-            voxel_size=voxel_size,
-            point_cloud_range=point_cloud_range,
-            out_size_factor=out_size_factor,
-        ),
-    ),
-    test_cfg=dict(
-        pts=dict(
-            grid_size=grid_size,
-            out_size_factor=out_size_factor,
-            pc_range=point_cloud_range,
-            voxel_size=voxel_size,
-            # No filter by range
-            post_center_limit_range=[-200.0, -200.0, -10.0, 200.0, 200.0, 10.0],
-        ),
-    ),
 )
-
-randomness = dict(seed=0, diff_rank_seed=False, deterministic=True)
-
-lr = 3e-4
-param_scheduler = [
-    # learning rate scheduler
-    # During the first (max_epochs * 0.3) epochs, learning rate increases from 0 to lr * 10
-    # during the next epochs, learning rate decreases from lr * 10 to
-    # lr * 1e-4
-    dict(
-        type="CosineAnnealingLR",
-        T_max=8,
-        eta_min=lr * 10,
-        begin=0,
-        end=8,
-        by_epoch=True,
-        convert_to_iter_based=True,
-    ),
-    dict(
-        type="CosineAnnealingLR",
-        T_max=22,
-        eta_min=lr * 1e-4,
-        begin=8,
-        end=max_epochs,
-        by_epoch=True,
-        convert_to_iter_based=True,
-    ),
-    # momentum scheduler
-    # During the first (0.3 * max_epochs) epochs, momentum increases from 0 to 0.85 / 0.95
-    # during the next epochs, momentum increases from 0.85 / 0.95 to 1
-    dict(
-        type="CosineAnnealingMomentum",
-        T_max=8,
-        eta_min=0.85 / 0.95,
-        begin=0,
-        end=8,
-        by_epoch=True,
-        convert_to_iter_based=True,
-    ),
-    dict(
-        type="CosineAnnealingMomentum",
-        T_max=22,
-        eta_min=1,
-        begin=8,
-        end=max_epochs,
-        by_epoch=True,
-        convert_to_iter_based=True,
-    ),
-]
-
-# runtime settings
-# Run validation for every val_interval epochs before max_epochs - 10, and run validation every 2 epoch after max_epochs - 10
-train_cfg = dict(
-    by_epoch=True, max_epochs=max_epochs, val_interval=val_interval, dynamic_intervals=[(max_epochs - 5, 1)]
-)
-val_cfg = dict()
-test_cfg = dict()
-
-optimizer = dict(type="AdamW", lr=lr, weight_decay=0.01)
-clip_grad = dict(max_norm=15, norm_type=2)  # max norm of gradients upper bound to be 15 since amp is used
 
 optim_wrapper = dict(
     type="AmpOptimWrapper",
@@ -441,48 +131,13 @@ optim_wrapper = dict(
     clip_grad=clip_grad,
     # Update it accordingly
     loss_scale={
-        "init_scale": 2.0**12,  # intial_scale: 256
-        "growth_interval": 600,
+        "init_scale": 2.0**8,  # intial_scale: 256
+        "growth_interval": 2000,
     },
 )
 
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (2 GPUs) x (8 samples per GPU).
-# auto_scale_lr = dict(enable=False, base_batch_size=32)
-auto_scale_lr = dict(enable=False, base_batch_size=train_gpu_size * train_batch_size)
-
-# Only set if the number of train_gpu_size more than 1
-if train_gpu_size > 1:
-    sync_bn = "torch"
-
-# vis_backends = [
-#     dict(type="LocalVisBackend"),
-#     dict(type="TensorboardVisBackend"),
-#     # Update info accordingly
-#     dict(
-#         type="SafeMLflowVisBackend",
-#         exp_name="(UserName) CenterPoint",
-#         run_name="CenterPoint base",
-#         tracking_uri="http://localhost:5000",
-#         artifact_suffix=(),
-#     ),
-# ]
-# visualizer = dict(type="Det3DLocalVisualizer", vis_backends=vis_backends, name="visualizer")
-
-logger_interval = 50
-default_hooks = dict(
-    logger=dict(type="LoggerHook", interval=logger_interval),
-    checkpoint=dict(type="CheckpointHook", interval=1, max_keep_ckpts=10, save_best="NuScenes metric/T4Metric/mAP"),
-)
-
-custom_hooks = [
-    dict(type="MomentumInfoHook"),
-    dict(type="LossScaleInfoHook"),
-]
 
 # Update the load_from path accordingly
-load_from = None
+load_from = "<best_checkpoint>"
 
 activation_checkpointing = ["pts_backbone"]
