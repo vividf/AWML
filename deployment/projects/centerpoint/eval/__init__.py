61d95fe47cb8c7e7eaa8a18ec1899ac94c690167
vividf <yihsiang.fang@tier4.jp>
2026-03-11 13:16:04 +0900

    chore: remove init
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- deployment/projects/centerpoint/eval/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Evaluation and metrics utilities for CenterPoint deployment."""
-
-from deployment.projects.centerpoint.eval.evaluator import CenterPointEvaluator
-from deployment.projects.centerpoint.eval.metrics_utils import extract_t4metric_v2_config
-
-__all__ = [
-    "CenterPointEvaluator",
-    "extract_t4metric_v2_config",
-]
